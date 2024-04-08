54a894d01519fd78d8e8faf71977398cb9f4a260
Xiang Guan Deng <xiangguand@gmail.com>
2024-04-08 03:57:12 +0000

    update test script, cpp call c function, add cmake toolchain

---
+++ cmake/gnu.cmake
@@ -0,0 +1,59 @@
+# Set the name of the toolchain
+set(CMAKE_SYSTEM_NAME ${CMAKE_HOST_SYSTEM_NAME})
+
+# Set the compiler to GCC
+set(CMAKE_C_COMPILER gcc)
+set(CMAKE_CXX_COMPILER g++)
+
+# Set other compiler flags as needed.
+set(CMAKE_C_FLAGS 
+    -Waddress
+    -Waggregate-return
+    -Wformat-nonliteral
+    -Wformat-security
+    -Wformat
+    -Winit-self
+    -Wmissing-declarations
+    -Wmissing-include-dirs
+    -Wno-multichar
+    -Wno-parentheses
+    -Wno-type-limits
+    -Wno-unused-parameter
+    -Wunreachable-code
+    -Wwrite-strings
+    -Wpointer-arith
+    # -Werror
+    -Wall
+    -pedantic
+    -Wall
+    -Wfatal-errors
+    -Wextra
+    -Wno-unused-parameter
+    -Wno-unused-variable
+)
+
+set(CMAKE_CXX_FLAGS 
+    -Waddress
+    -Waggregate-return
+    -Wformat-nonliteral
+    -Wformat-security
+    -Wformat
+    -Winit-self
+    -Wmissing-declarations
+    -Wmissing-include-dirs
+    -Wno-multichar
+    -Wno-parentheses
+    -Wno-type-limits
+    -Wno-unused-parameter
+    -Wunreachable-code
+    -Wwrite-strings
+    -Wpointer-arith
+    # -Werror
+    -Wall
+    -pedantic
+    -Wall
+    -Wfatal-errors
+    -Wextra
+    -Wno-unused-parameter
+    -Wno-unused-variable
+)
--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,50 @@
 
 dut=./build/main
 
-$dut
+set -e
 
-exit $?
+test_function() {
+    echo "========================================================="
+    local build_cmd="$1"
+    local expect_output="$2"
+    echo "build command \"$build_cmd\""
+    rm -rf build
+    $build_cmd
+    local exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        echo "Fail with command \"$(build_cmd)\""
+    fi
+
+    echo "Build target $dut"
+    ninja -C build -v all
+    local exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        echo "Fail with command \"$(build_cmd)\""
+    fi
+
+    echo "===== Execute $dut ====="
+    local output=$("$dut")
+    if [ "$output" == "$expect_output" ]; then
+        echo "The output strings are equal"
+    else
+        echo "The output strings are not equal, expect \"$expect_output\", return \"$output\""
+        exit 1
+    fi
+    echo "========================================================="
+}
+
+cmake_build() {
+    local clang_flag="$1"
+    local build_type="$2"
+    local expect_output="$3"
+    
+    test_function "cmake -S . -B build -G Ninja -DUSE_CLANG=$clang_flag -DCMAKE_BUILD_TYPE=$build_type" "$expect_output"
+}
+
+cmake_build ON Debug "Hello world 30,CLANG"
+cmake_build OFF Release "Hello world 30,GNU"
+cmake_build ON Debug "Hello world 30,CLANG"
+cmake_build OFF Release "Hello world 30,GNU"
+
+echo "===== All test cases PASS ====="
 
